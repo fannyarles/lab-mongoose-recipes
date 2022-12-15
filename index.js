f63d92d5f7c242623d672e7229a57a5423cede24
fannyarles <fanny.arles@orange.fr>
2022-12-15 15:53:15 +0100

    Completed lab

--- index.js
+++ index.js
@@ -17,6 +17,38 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    const myRecipe = {
+      title: 'Banana Pancakes',
+      level: 'Easy Peasy',
+      ingredients: [
+        '1 1/2 cups (180g) all-purpose flour',
+        '2 Tablespoons (25g) sugar (optional)',
+        '2 1/2 teaspoons baking powder',
+        '1 pinch cinnamon (optional)',
+        '1 pinch salt',
+        '2 medium ripe bananas',
+        '1 cup (240 ml) milk',
+        '2 large eggs',
+        '1/4 cup (60 ml) canola oil'],
+      cuisine: 'American',
+      dishType: 'breakfast',
+      image: 'https://www.delscookingtwist.com/wp-content/uploads/2020/03/Easy-Fluffy-Banana-Pancakes_7.jpg',
+      duration: 20,
+      creator: 'Fanny Arles'
+    }
+
+    Recipe.create(myRecipe)
+      .then(createdRecipe => console.log(`${createdRecipe.title} successfully created!`))
+      .then(() => Recipe.insertMany(data))
+      .then((addedRecipes) => addedRecipes.forEach(recipe => console.log(`New recipe added: ${recipe.title}`)))
+      .then(() => Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }))
+      .then(() => console.log(`Rigatoni recipe successfully updated!`))
+      .then(() => Recipe.findOneAndDelete( { title: 'Carrot Cake' } ))
+      .then(() => console.log(`Carrot Cake recipe successfully deleted.`))
+      .then(() => mongoose.connection.close())
+      .catch(err => console.log(err));
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,36 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  }, 
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: [ String ]
+  },
+  cuisine: String,
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    default: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: new Date()
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
